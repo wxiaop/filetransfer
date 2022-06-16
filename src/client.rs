df375e2dfc52de9f37c872b401ff03d7fd23353e
nszwxiaop <1091654568@qq.com>
2022-06-16 16:18:01 +0800

    UDP不稳定传输

--- src/client.rs
+++ src/client.rs
@@ -1,15 +1,17 @@
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::fs;
-use std::io::{self, prelude::*, BufReader, BufWriter};
-use std::net::TcpStream;
+use std::io::{self, prelude::*, BufReader};
+use std::net::UdpSocket;
 const EXIT: &str = "exit()";
 const PUT: &str = "put";
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     let ipaddr = &args[1].clone();
+    let socket = UdpSocket::bind("0.0.0.0:9976")?;
     loop {
-        let mut stream = TcpStream::connect(ipaddr)?;
+        socket.connect(ipaddr).expect("connection failed!");
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read!!");
         let input: Vec<_> = input.trim_end().split_whitespace().collect();
@@ -19,51 +21,57 @@ fn main() -> io::Result<()> {
             return Ok(());
         } else if input[0] == PUT {
             let start = std::time::Instant::now();
-            open_read_file(&mut stream, input[1]);
-            stream.shutdown(std::net::Shutdown::Both)?;
+            open_read_file(&socket, input[1]);
             let duration = start.elapsed();
             println!("执行时间为: {:?}", duration);
         }
     }
-
-    // println!("w ==> {:#?}", w);
-    // let start = std::time::Instant::now();
-    //todo
-    // let duration = start.elapsed();
-    // println!("执行时间为: {:?}", duration);
-    // Ok(())
 }
 
-fn open_read_file(stream: &mut TcpStream, filename: &str) {
-    // let mut stream = BufWriter::new(stream);
-    // let mut file_s = fs::File::open(filename).expect("Failed to open file!!");
-    // println!("{:#?}", file_s);
-    // stream.write(filename.as_bytes()).unwrap();
-    // let mut w = [0; 49812];
-    // loop {
-    //     let byte_s = file_s.read(&mut w[..]).unwrap();
-    //     if byte_s != 0 {
-    //         println!("==========> 正在上传 ...");
-    //         stream.write(&w[..byte_s]).unwrap();
-    //         // stream.flush().unwrap();
-    //     } else {
-    //         println!("=========== 上传完成 ===========");
-    //         break;
-    //     }
-    // }
+fn open_read_file(socket: &UdpSocket, filename: &str) {
     let mut w = [0; 49812];
     let file_s = fs::File::open(filename).expect("Failed to open file!!");
-    stream.write(filename.as_bytes()).unwrap();
+    socket.send(filename.as_bytes()).unwrap();
     let mut reader = BufReader::new(file_s);
+    let mut count = 0;
     loop {
         let byte_s = reader.read(&mut w[..]).unwrap();
+        count += 1;
+        println!("{}", count);
         if byte_s != 0 {
             println!("==========> 正在上传 ...");
-            stream.write(&w[..byte_s]).unwrap();
+            socket.send(&w[..byte_s]).unwrap();
             // stream.flush().unwrap();
         } else {
+            socket.send("UPLOAD_DONE".as_bytes()).unwrap();
             println!("=========== 上传完成 ===========");
-            break;
+            let mut buffer = [0; 2];
+            socket.recv_from(&mut buffer).unwrap();
+            let res = String::from_utf8(buffer.to_vec()).unwrap() == "OK";
+            let res2 = buffer == "OK".as_bytes();
+            let res3 = "OK" == String::from_utf8_lossy(&buffer);
+            println!("recv : {:?}", buffer);
+            println!("recv : {}", String::from_utf8(buffer.to_vec()).unwrap());
+            println!("recv : {}", res);
+            println!("recv : {}", res2);
+            println!("recv : {}", res3);
+            if res {
+                println!("coming recv : {}", String::from_utf8_lossy(&buffer));
+                break;
+            }
         }
     }
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Filedata {
+    id: i32,
+    byte: Vec<u8>,
+}
+///struct => [u8]
+pub unsafe fn struct_to_u8<T: Sized>(src: &T) -> &[u8] {
+    ::std::slice::from_raw_parts((src as *const T) as *const u8, ::std::mem::size_of::<T>())
+}
+/// [u8] => struct
+pub unsafe fn u8_to_struct(src: Vec<u8>) -> Filedata {
+    std::ptr::read(src.as_ptr() as *const _)
 }
--- src/server.rs
+++ src/server.rs
@@ -1,53 +1,87 @@
-use std::env;
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io::{self, prelude::*, BufWriter};
-use std::net::{TcpListener, TcpStream};
+use std::net::UdpSocket;
 use std::thread;
 
 fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:9977")?;
-    // let mut thread_list: Vec<thread::JoinHandle<()>> = Vec::new();
+    let socket = UdpSocket::bind("0.0.0.0:9977")?;
 
-    let mut thread_list = vec![];
-    for stream in listener.incoming() {
-        let stream = stream.expect("failed");
+    loop {
+        let socket = socket.try_clone()?;
+        let mut thread_list = vec![];
         let handle = thread::spawn(move || {
-            receive_file(&stream);
+            receive_file(&socket).expect("fail");
         });
         thread_list.push(handle);
+
+        for handle in thread_list {
+            handle.join();
+        }
     }
-    for handle in thread_list {
-        handle.join();
-    }
-    Ok(())
 }
 
 //接收文件
-fn receive_file(mut stream: &TcpStream) -> io::Result<()> {
+fn receive_file(socket: &UdpSocket) -> io::Result<()> {
     let mut buf = [0; 49812];
     // 第一次读取 读取文件名
-    let byte_read = stream.read(&mut buf)?;
-
-    let filename = String::from_utf8_lossy(&buf[..byte_read]);
-
+    let (amt, src) = socket.recv_from(&mut buf)?;
+    let filename = String::from_utf8_lossy(&buf[..amt]);
     // let filename: Vec<_> = filename.split(".").collect();
     // let namesub = format!("{}{}", timestamp(), filename);
-    println!("filename => {} ", filename.to_string());
+    // println!("filename => {} ", filename.to_string());
     let file = fs::File::create(filename.to_string()).expect("Failed to create file!");
     let mut writer_buf = BufWriter::new(file);
+    let mut count = 0;
     loop {
-        let byte_read = stream.read(&mut buf)?;
-        if byte_read != 0 {
-            println!("=========> 正在保存...");
-            writer_buf.write_all(&buf[..byte_read])?;
-            writer_buf.flush()?;
-        } else {
+        let (amt, src) = socket.recv_from(&mut buf)?;
+        println!("loop => {}", amt);
+        count += 1;
+        println!("times: => {}", count);
+
+        // if amt == 49812 {
+        //     println!("=========> 正在保存...");
+        //     writer_buf.write_all(&buf[..amt])?;
+        //     writer_buf.flush()?;
+        // } else {
+        //     println!("========= 保存完成 =========");
+        //     writer_buf.write_all(&buf[..amt])?;
+        //     writer_buf.flush()?;
+        //     socket.send_to("OK".as_bytes(), &src)?;
+        //     break;
+        // }
+        if "UPLOAD_DONE".as_bytes() == &buf[..amt] {
             println!("========= 保存完成 =========");
+            socket.send_to("OK".as_bytes(), &src)?;
             break;
+        } else {
+            println!("=========> 正在保存...");
+            writer_buf.write_all(&buf[..amt])?;
+            writer_buf.flush()?;
         }
     }
     Ok(())
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Filedata {
+    id: i32,
+    byte: Vec<u8>,
+}
+// #[repr(C)]
+// #[derive(Debug)]
+// pub struct Filedata<'a> {
+//     id: i32,
+//     byte: &'a [u8],
+// }
+// ///struct => [u8]
+// pub unsafe fn struct_to_u8<T: Sized>(src: &T) -> &[u8] {
+//     ::std::slice::from_raw_parts((src as *const T) as *const u8, ::std::mem::size_of::<T>())
+// }
+// /// [u8] => struct
+// pub unsafe fn u8_to_struct(src: Vec<u8>) -> Filedata<'static> {
+//     std::ptr::read(src.as_ptr() as *const _)
+// }
 ///获取时间戳
 fn _timestamp() -> i64 {
     let start = std::time::SystemTime::now();
